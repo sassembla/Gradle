274f727fda8a00b12eae48ac8ce8d197a349c639
sassembla <sassembla@hotmail.com>
2012-11-04 14:11:37 +0900

    雑多な実験の結果、mainthreadへの復帰が出来るようになった。
    sublime.set_timeout　で、main threadでの実行が予定される。
    特定のメソッドをmain thread　から直呼びできる。

--- testing.py
+++ testing.py
@@ -5,7 +5,13 @@ import os
 import sublime
 import sublime_plugin
 import subprocess
-import threading 
+import threading
+
+def main_thread(callback, *args, **kwargs):
+#     # sublime.set_timeout gets used to send things onto the main thread
+#     # most sublime.[something] calls need to be on the main thread
+#     sublime.set_timeout(functools.partial(callback, *args, **kwargs), 0)
+  print "callback!"
 
 # 色んな実験を行う。
 class Testing(sublime_plugin.TextCommand):
@@ -14,6 +20,8 @@ class Testing(sublime_plugin.TextCommand):
     gradle = sublime.load_settings("Gradle.sublime-settings").get("command").get('gradle')
     build = sublime.load_settings("Gradle.sublime-settings").get("command").get('build')
 
+    sublime.status_message('gradle '+build+" running...")
+
     #単純に実行、ただしロックしない。
     # self.view.window().run_command('exec', {'cmd': [gradle, 'build'], 'quiet': True})
 
@@ -21,18 +29,47 @@ class Testing(sublime_plugin.TextCommand):
     # subprocess.call(gradle + " " + 'build', shell=True)
 
     # スレッドを作成してスレッド内から実行したところ、問題ない！
-    thread = BuildThread(self.view, gradle + " " + build)
+    thread = BuildThread(self, gradle + " " + build)
     thread.start()
 
+# スレッドの監視は重い。
+    # プールを監視
+    # self.handle_threads(thread, "aaa")
+
+  def masterC(self):
+    print "here comes"
+    # 呼べるけどメインに帰って来れない。やっぱりなー
+    sublime.status_message('gradle '+"build"+" done.")
+
+  # def handle_threads(self, thread, message="def"):
+  #   print "haha?" ,message
+  #   # if thread.is_alive():
+  #   #   print "still alive"
+
+  #   if thread.result == False:
+  #     # t = threading.Timer(1.0, self.handle_threads(thread))
+  #     # t.start()
+  #     print "still running"
+    
+  #   print "handle over"
     
 class BuildThread(threading.Thread):
-  def __init__(self, view, command):
-    self.view = view
+  def __init__(self, master, command):
+    self.master = master
     self.command = command
+    
     threading.Thread.__init__(self)
 
   def run(self):
     print "run started ",self.command
     subprocess.call(self.command, shell=True)
+
+    # def切っても駄目
+    # 
+
+    sublime.set_timeout(
+                    lambda: main_thread(self.command, self.master.masterC()),
+                    0)
+
+    # self.master.masterC()
     print "run end ",self.command
-    
